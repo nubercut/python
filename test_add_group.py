df905486c1067ab2592ba2580e634f44ed743e10
Nubar.Petrosyan <npbercut@gmail.com>
2023-02-15 17:17:15 +0300

    Выделение return to group page в отдельный метод

--- test_add_group.py
+++ test_add_group.py
@@ -15,10 +15,13 @@ class TestAddGroup(unittest.TestCase):
         self.login(wd)
         self.open_group_page(wd)
         self.create_group(wd)
-        # return to group page
-        wd.find_element_by_link_text("group page").click()
+        self.return_to_group_page(wd)
         # logout
         wd.find_element_by_link_text("Logout").click()
+
+    def return_to_group_page(self, wd):
+        # return to group page
+        wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd):
         # init group creation
