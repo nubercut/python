b84f449a70849d608eda12513014cbe1a4dbe79e
Nubar.Petrosyan <npbercut@gmail.com>
2023-02-15 17:44:13 +0300

    Добавил тест по созданию пустой группы

--- test_add_group.py
+++ test_add_group.py
@@ -17,6 +17,14 @@ class TestAddGroup(unittest.TestCase):
         self.create_group(wd, name="sdfg", header="sdfg", footer="sdg")
         self.return_to_group_page(wd)
         self.logout(wd)
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_group_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_group_page(wd)
+        self.logout(wd)
 
     def logout(self, wd):
         # logout
