8135e8d08363352eb6ee7be370d967fd4dfe190f
Nubar.Petrosyan <npbercut@gmail.com>
2023-03-02 19:14:58 +0300

    Оптимизирован ввод данных в форму

--- fixture/group.py
+++ fixture/group.py
@@ -11,58 +11,48 @@ class GroupHelper:
         wd = self.app.wd
         self.open_group_page()
         # init group creation
-        self.init_group_creation(wd)
+        wd.find_element_by_name("new").click()
         # fill group form
-        self.fill_group_form(group, wd)
+        self.fill_group_form(group)
         # submit group creation
-        self.submit_group_creation(wd)
+        wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
 
-    def fill_group_form(self, group, wd):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
-    def init_group_creation(self, wd):
-        wd.find_element_by_name("new").click()
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
         # select first group
-        self.select_first_group(wd)
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
-    def modify_first_group(self, group):
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        # select first group
-        self.select_first_group(wd)
-        # submit modyfying
-        self.edit_first_group(wd)
-        self.fill_group_form(group, wd)
-        # submit group creation
-        self.update_first_group(wd)
-        self.return_to_group_page()
+        self.select_first_group()
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
 
     def update_first_group(self, wd):
         wd.find_element_by_name("update").click()
 
-    def edit_first_group(self, wd):
-        wd.find_element_by_name("edit").click()
-
-    def select_first_group(self, wd):
+    def select_first_group(self):
+        wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
     def open_group_page(self):
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,7 +1,10 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="This", header="Group is", footer="Modified"))
+def test_modify_group_name(app):
+    app.group.modify_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.group.modify_first_group(Group(header="New header"))
     app.session.logout()
