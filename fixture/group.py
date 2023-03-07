96ad4320105398d6e6ed17ab1fe31f69372cb1a6
Nubar.Petrosyan <npbercut@gmail.com>
2023-03-07 18:18:20 +0300

    Реализована проверка валидности фикстуры и проверка состояния сессии

--- fixture/group.py
+++ fixture/group.py
@@ -3,18 +3,14 @@ class GroupHelper:
         self.app = app
 
     def return_to_group_page(self):
-        # return to group page
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
 
     def create(self, group):
         wd = self.app.wd
         self.open_group_page()
-        # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
         self.fill_group_form(group)
-        # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
@@ -35,9 +31,7 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        # select first group
         self.select_first_group()
-        # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
