f503be0cd532042539108191f676da8425f99758
Nubar.Petrosyan <npbercut@gmail.com>
2023-03-27 22:47:32 +0300

    test_phones_on_contact_view_page (исправил ошибку)

--- test/test_phones.py
+++ test/test_phones.py
@@ -10,7 +10,7 @@ def test_phones_on_homepage(app):
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.all_phones_from_homepage == merge_phones_like_on_homepage(contact_from_edit_page)
+    assert contact_from_view_page.tel_home ==  contact_from_edit_page.tel_home
     assert contact_from_view_page.tel_work == contact_from_edit_page.tel_work
     assert contact_from_view_page.tel_mobile == contact_from_edit_page.tel_mobile
     assert contact_from_view_page.phone_2 == contact_from_edit_page.phone_2
