b980da96899c93bffc1a3fc7ec4c6bf71bf094ee
Nubar.Petrosyan <npbercut@gmail.com>
2023-03-28 21:33:13 +0300

    Параметризация тестов создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,7 +5,7 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10
+    symbols = string.ascii_letters + string.digits + "" * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
